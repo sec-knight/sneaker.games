6345d68846258bc7861ddea75585387feb94416e
DragonWave2949 <92043420+sec-knight@users.noreply.github.com>
2026-08-13 15:16:25 -0500

    Fix writing quote contrast

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 document.getElementById('year').textContent = new Date().getFullYear();
+
+document.querySelectorAll('.wisdom-entry blockquote').forEach((quote) => {
+  quote.style.color = '#f8f4f3';
+});
